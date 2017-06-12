fd684741de620fb75c36942775050bcb563b3ff8
Tensei <davuttensei@gmail.com>
2017-06-13 00:10:59 +0200

    updated nugets fixed search not hiding listitems

--- GbfRaidfinder.Data/RaidListItem.cs
+++ GbfRaidfinder.Data/RaidListItem.cs
@@ -1,12 +1,15 @@
-﻿using PropertyChanged;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PropertyChanged;
 
 namespace GbfRaidfinder.Data {
-    [ImplementPropertyChanged]
-    public class RaidListItem {
+    public class RaidListItem : INotifyPropertyChanged {
         public string English { get; set; }
         public string Japanese { get; set; }
         public string Image { get; set; }
         public bool Following { get; set; }
         public bool Visibility { get; set; } = true;
+        public event PropertyChangedEventHandler PropertyChanged;
+        
     }
 }
